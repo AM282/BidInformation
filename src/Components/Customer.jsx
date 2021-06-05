77e332d8da1d59ff8513c4d1f059156b595f321b
Anju Maurya <anju.maurya@globallogic.com>
2021-06-05 15:19:10 +0530

    Refactored code

--- src/Components/Customer.jsx
+++ src/Components/Customer.jsx
@@ -95,77 +95,11 @@ const Customer = () => {
             ]
         }
     ],[]);
-    const{
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-        getRowProps,
-        page,
-        pageOptions,
-        state:{pageIndex,pageSize},
-        previousPage,
-        nextPage,
-        canPreviousPage,
-        canNextPage
-    }=useTable(
-        {
-            columns,
-            data,
-            initialState:{pageSize:5},
-        },
-        useSortBy,
-        usePagination
-        );
     return (
         <div className="container">
-            <div className="row">
-                <table className="table table-bordered">
-                    <caption style={{captionSide: "top"}}>Customers List</caption>
-                    
-                    <thead>
-                        {headerGroups.map(headerGroup=>(
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map(column=>(
-                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                        {column.render('Header')}
-                                        <span>
-                                            {column.isSorted?(column.isSortedDesc?'d':'a'):''}
-                                        </span>
-                                    </th>
-                                ))}
-                            </tr>
-                        ))}
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {page.map(row=>{
-                            prepareRow(row)
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map(cell=>{
-                                        return <td {...cell.getCellProps()}>
-                                            {cell.render('Cell')}
-                                        </td>
-                                    })}
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-                <div>
-                    <button onClick={()=>previousPage()} disabled={!canPreviousPage}>Previous Page</button>
-                    <button onClick={()=>nextPage()} disabled={!canNextPage}>Next Page</button>
-                    <div>
-                        Page{' '}
-                        <em>
-                            {pageIndex+1} of {pageOptions.length}
-                        </em>
-                    </div>
-                </div>
-            </div>
+                <DisplayCustomers columns={columns} data={data}/>
         </div>
-    )
+    );
 }
 
 export default Customer;
--- src/Components/DisplayCustomers.jsx
+++ src/Components/DisplayCustomers.jsx
@@ -1,23 +1,78 @@
 import React, {useState} from 'react';
+import { usePagination, useSortBy, useTable } from 'react-table';
 
 
-const DisplayCustomers = (props) => {
-    let fullname=`${props.firstName} ${props.lastName}`
-    let hasPremium=`${props.hasPremium}`;
-    let bids=props.bid;
-    let maxbid=bids.length!==0?Math.max.apply(Math, bids.map(function(bid) { return bid.amount; })):0;
-    let minbid=bids.length!==0?Math.min.apply(Math,bids.map(function(bid){return bid.amount;})):0;
-
+const DisplayCustomers = ({ columns, data }) => {
+ 
+    const{
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+        getRowProps,
+        page,
+        pageOptions,
+        state:{pageIndex,pageSize},
+        previousPage,
+        nextPage,
+        canPreviousPage,
+        canNextPage
+    }=useTable(
+        {
+            columns,
+            data,
+            initialState:{pageSize:5},
+        },
+        useSortBy,
+        usePagination
+        );
     return (
-        
-            <tr>
-                <td>{fullname}<img src={`${props.avatarUrl}`} alt="Avatar"></img></td>
-                <td>{props.email}</td>
-                <td>{props.phone}</td>
-                <td>{hasPremium}</td>
-                <td>{maxbid}</td>
-                
-            </tr>
+                <div className='row'>
+                        <table className="table table-bordered">
+                    <caption style={{captionSide: "top"}}>Customers List</caption>
+                    
+                    <thead>
+                        {headerGroups.map(headerGroup=>(
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map(column=>(
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                        {column.render('Header')}
+                                        <span>
+                                            {column.isSorted?(column.isSortedDesc?'d':'a'):''}
+                                        </span>
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+
+                            {page.map(row=>{
+                                prepareRow(row)
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map(cell=>{
+                                        return <td {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    })}
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                <div>
+                    <button onClick={()=>previousPage()} disabled={!canPreviousPage}>Previous Page</button>
+                    <button onClick={()=>nextPage()} disabled={!canNextPage}>Next Page</button>
+                    <div>
+                        Page{' '}
+                        <em>
+                            {pageIndex+1} of {pageOptions.length}
+                        </em>
+                    </div>
+                </div>
+            </div>
 
     );
 }
