54107e510e48c58d497e19d921e1c29401dea179
Anju Maurya <anju.maurya@globallogic.com>
2021-06-05 12:57:45 +0530

    Using react-table

--- src/Components/Customer.jsx
+++ src/Components/Customer.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { useTable } from 'react-table';
 import DisplayCustomers from './DisplayCustomers';
 
 const Customer = () => {
     let [customers, setcustomers] = useState([]);
+    let [toggler,settoggler]=useState(true);
     useEffect(()=>{
         fetch(`https://intense-tor-76305.herokuapp.com/merchants`).then((res) => {
             if (res.status === 200) {
@@ -20,6 +22,73 @@ const Customer = () => {
         });
 
     },[]);
+    let getData = ()=>{
+        return customers.map(customer =>{
+            let temp=Object.assign({},customer);
+            let bids=temp.bids;
+            temp.bid=0;
+            if(toggler===true)
+            {
+                bids.map(bidd=>(
+                    temp.bid=Math.max(bidd.amount,temp.bid)
+                ))
+            }
+            else
+            {
+                bids.map(bidd=>(
+                    temp.bid=Math.min(bidd.amount,temp.bid)
+                ))
+            }
+
+            return temp;
+        })
+    }
+    const data=React.useMemo(()=>
+    getData(),[customers]);
+    const columns=React.useMemo(()=>[
+        {
+            Header:'Name',
+            columns:[
+                {
+                    Header:'firstName',
+                    accessor:'firstname'
+                },
+                {
+                    Header:'lastName',
+                    accessor:'lastname'
+                }
+            ],
+        },
+        {    Header:"Customer Info",
+            columns:[
+                {
+                    Header:'Email',
+                    accessor:'email'
+                },
+                {
+                    Header:'Phone',
+                    accessor:'phone'
+                }
+            ]
+        },
+        {
+            Header:'Bid Information',
+            columns:[
+                {
+                    Header:'Max/Min Bid',
+                    id:'bids',
+                    accessor:'bid'
+                }
+            ]
+        }
+    ],[]);
+    const{
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    }=useTable({columns,data});
     return (
         <div className="container">
             <div className="row">
@@ -27,7 +96,14 @@ const Customer = () => {
                     <caption style={{captionSide: "top"}}>Customers List</caption>
                     
                     <thead>
-                        <tr>
+                        {headerGroups.map(headerGroup=>(
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map(column=>(
+                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                ))}
+                            </tr>
+                        ))}
+                        {/* <tr>
 
                             <th scope="col">
                                 Customer Name
@@ -38,10 +114,22 @@ const Customer = () => {
                             <th scope="col">
                                 Max/Min Bid
                             </th>
-                        </tr>
+                        </tr> */}
                     </thead>
-                    <tbody>
-                        {customers.map((customer) => (
+                    <tbody {...getTableBodyProps()}>
+                        {rows.map(row=>{
+                            prepareRow(row)
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map(cell=>{
+                                        return <td {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    })}
+                                </tr>
+                            )
+                        })}
+                        {/* {customers.map((customer) => (
                             <DisplayCustomers
                                 key={customer.id}
                                 id={customer.id}
@@ -53,7 +141,7 @@ const Customer = () => {
                                 hasPremium={customer.hasPremium}
                                 bid={customer.bids}
                             />
-                        ))}
+                        ))} */}
                     </tbody>
                 </table>
             </div>
